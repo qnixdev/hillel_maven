89df6c8b2512574ae24011206ebd3cd5b928a216
qnixdev <qnixdev@gmail.com>
2022-05-13 02:58:15 +0300

    fix task

--- src/main/java/com/hw15/FileManager.java
+++ src/main/java/com/hw15/FileManager.java
@@ -1,66 +1,46 @@
 package com.hw15;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class FileManager {
-    public void parser(String filePath) {
-        try {
-            var fileReader = new FileReader(filePath);
-            var bufferedReader = new BufferedReader(fileReader);
-
-            bufferedReader.lines()
-                .flatMap(s -> Arrays.stream(s.split(" ")))
+    public void fileParser(Path filePath) {
+        try (
+            var fr = new BufferedReader(new FileReader(filePath.toString()));
+            var fw = new BufferedWriter(new FileWriter(this.buildResultPath(filePath)))
+        ) {
+            List<Map.Entry<String, Long>> list = fr.lines()
+                .flatMap(s -> Arrays.stream(s.split("[^А-Яа-я]+")))
                 .map(String::toLowerCase)
-                .map(s -> s
-                    .replace(",", "")
-                    .replace(".", "")
-                    .replace("?", "")
-                    .replace("!", "")
-                    .replace(":", "")
-                    .replace(";", "")
-                    .replace("-", "")
-                    .replace("‑", "")
-                    .replace("_", "")
-                    .replace("'", "")
-                    .replace("\"", "")
-                    .replace("’", "")
-                    .replace("«", "")
-                    .replace("»", "")
-                    .replace("…", "")
-                    .replace("[", "")
-                    .replace("]", "")
-                    .replace("(", "")
-                    .replace(")", "")
-                    .replace("{", "")
-                    .replace("}", "")
-                    .replace("1", "")
-                    .replace("2", "")
-                    .replace("3", "")
-                    .replace("4", "")
-                    .replace("5", "")
-                    .replace("6", "")
-                    .replace("7", "")
-                    .replace("8", "")
-                    .replace("9", "")
-                    .replace("0", "")
-                    .replace("русс", "")
-                    .replace("рассия", "")
-                )
                 .filter(s -> s.length() > 3)
+                .filter(s -> !s.contains("русс"))
+                .filter(s -> !s.contains("рассия"))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .forEach(System.out::println);
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .toList()
             ;
-        } catch (FileNotFoundException e) {
+
+            var iterator = list.iterator();
+
+            while (iterator.hasNext()) {
+                fw.write(String.valueOf(iterator.next()) + '\n');
+            }
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public String buildResultPath(Path path) {
+        var filePath = path.toString();
+
+        return filePath.substring(0, filePath.indexOf('.')) + "_result.txt";
     }
 }
