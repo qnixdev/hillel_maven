3e3432aa540b8c7dc06e7c1aa6c758b2f49c8198
qnixdev <qnixdev@gmail.com>
2022-06-13 00:11:39 +0300

    add driver & fix resource

--- src/main/java/com/hw20/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hw20;
-
-public class Main {
-    public static void main(String[] args) {
-        Library library = new Library();
-        library.run();
-    }
-}
--- src/main/resources/hw20/init.sql
+++ src/main/resources/hw20/init.sql
@@ -1,19 +1,27 @@
+-- auto-generated definition
+CREATE SEQUENCE author_id_seq;
+ALTER SEQUENCE author_id_seq OWNER TO postgres;
+-- create table
 CREATE TABLE author
 (
-    id integer not null,
-    first_name varchar(63) not null,
-    last_name varchar(63) not null,
-    primary key (id)
+    id integer NOT NULL,
+    first_name varchar(63) NOT NULL,
+    last_name varchar(63) NOT NULL,
+    PRIMARY KEY (id)
 );
 ALTER TABLE author OWNER TO postgres;
 
+-- auto-generated definition
+CREATE SEQUENCE book_id_seq;
+ALTER SEQUENCE book_id_seq OWNER TO postgres;
+-- create table
 CREATE TABLE book
 (
-    id integer not null,
+    id integer NOT NULL,
     author_id integer,
-    title varchar(255) not null,
-    primary key (id),
-    constraint fk_cbe5a331f675f31b foreign key (author_id) references author
+    title varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_cbe5a331f675f31b FOREIGN KEY (author_id) REFERENCES author
 );
 ALTER TABLE book OWNER TO postgres;
 CREATE INDEX idx_cbe5a331f675f31b ON book (author_id);
